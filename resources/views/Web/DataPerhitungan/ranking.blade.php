ed0d703e1a724737f9c5fc656fea62cb6f9d3af9
Ario Sutrisno <sutrisnoario@gmail.com>
2021-07-25 13:00:10 +0700

    ranking view update

--- resources/views/Web/DataPerhitungan/ranking.blade.php
+++ resources/views/Web/DataPerhitungan/ranking.blade.php
@@ -12,72 +12,21 @@
 
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
-
             <!-- Main Content -->
             <div id="content">
-
                 <!-- Topbar -->
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-
-                    <!-- Topbar Search -->
-                    <form
-                        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                                aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
-
-                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
-                            </a>
-                            <!-- Dropdown - Messages -->
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                aria-labelledby="searchDropdown">
-                                <form class="form-inline mr-auto w-100 navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small"
-                                            placeholder="Search for..." aria-label="Search"
-                                            aria-describedby="basic-addon2">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
-                                                <i class="fas fa-search fa-sm"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
-
-                        
-
                         <!-- Nav Item - User Information -->
                         @include('Web.LayoutPage.profileHeader')
-
                     </ul>
 
                 </nav>
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
-                <div class="container-fluid"  style="overflow-x:auto;">
-                    
+                <div class="container-fluid" >
                     <!-- Page Heading -->
                     <h1 class="h3 mb-4 text-gray-800">Hasil Ranking </h1>
                     <div class="float-right">
@@ -85,29 +34,32 @@
                             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Cek Hasil</button>
                         </div> --}}
                     </div>
-                    
-                    <table class="table table-bordered text-center" id="dtHorizontalVerticalExample" width="100%">
-                        <thead class="table-warning">
-                            <tr>
-                                <th>NO</th>
-                                <th>Kode Alternatif</th>
-                                <th>Nama Alternatif</th>
-                                <th>Ranking</th>
-                            </tr>
+                    <br>
+                    <div class="container mt-4">
+                        <table id="example" class="table-success text-center" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>NO</th>
+                                    <th>Kode Alternatif</th>
+                                    <th>Nama Alternatif</th>
+                                    <th>Ranking</th>
+                                </tr>
                             </thead>
+                            <?php $no = 0; ?>
+                            <?php $no++ ?>
                             <tbody>
-                                <?php $no = 0; ?>
-                                <?php $no++ ?>
                                 @foreach ($data_ranking as $ranking)
-                                    <tr>
-                                        <td>{{ $no++ }}</td>
-                                        <td>{{ $ranking->alternatif }}</td>
-                                        <td>{{ $ranking->datalaptop }}</td>
-                                        <td>{{ $ranking->hasil_akhir }}</td>
-                                    <tr>
+                                <tr>
+                                    <td>{{ $no++ }}</td>
+                                    <td>{{ $ranking->alternatif }}</td>
+                                    <td>{{ $ranking->datalaptop }}</td>
+                                    <td>{{ $ranking->hasil_akhir }}</td>
+                                </tr>
                                 @endforeach
                             </tbody>
-                    </table>
+                            {{-- @endforeach --}}
+                        </table>
+                    </div>
                 </div>
                 <!-- /.container-fluid -->
 
@@ -115,7 +67,7 @@
             <!-- End of Main Content -->
             
             <!-- Footer -->
-            @include('Web.LayoutPage.footer')
+            {{-- @include('Web.LayoutPage.footer') --}}
             <!-- End of Footer -->
 
         </div>
@@ -155,19 +107,12 @@
 @endsection
 
 @section('script')
+
 <script>
-        
-        $(document).ready(function () {
-            $('#dtHorizontalVerticalExample').DataTable({
-            "scrollX": true,
-            "scrollY": 200,
-            });
-            $('.dataTables_length').addClass('bs-select');
-            });
-            
-
-            
-
-
+        $(document).ready(function() {
+            $('#example').DataTable( {
+                "pagingType": "full_numbers"
+            } );
+        } );
 </script>
 @endsection
